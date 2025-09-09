109d8a60a7d86a43fc2b64d73042ca08f8f80faa
Gabriel <gabrielroith@gmail.com>
2025-09-09 18:20:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ GROQ_API_KEY = st.secrets["GROQ_API_KEY"]
 client = Groq(api_key=GROQ_API_KEY)
 
 # Rutas y parámetros
-DATA_PATH = "data/peliculas_data.pkl.gz"
+DATA_PATH = "data/peliculas_data.zip"
 MODEL_NAME = "hiiamsid/sentence_similarity_spanish_es"
 LLM_MODEL = "llama-3.1-8b-instant"
 
@@ -187,5 +187,6 @@ if query:
 
         respuesta_final = consultar_llm(llm_client, query, recomendaciones)
         st.success(respuesta_final)
+
 
 
